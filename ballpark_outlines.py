ce5dc9208cfd0025d3d254209eefd5a8bc81ce8d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-08-30 14:29:39 -0600

    Update ballpark_outlines.py

--- ballpark_outlines.py
+++ ballpark_outlines.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 stadium_df = pd.read_csv('https://raw.githubusercontent.com/jldbc/pybaseball/master/pybaseball/data/mlbstadiums.csv').drop(columns=['Unnamed: 0'])
 
 # Function to plot stadium dimensions
-def stadium_plot(team, ax, scale_val_bat, segments=['outfield_outer','foul_lines','infield_inner','infield_outer']):
+def stadium_plot(team, ax, segments=['outfield_outer','foul_lines','infield_inner','infield_outer'], linewidth=2):
   stadium_graph_data = stadium_df.loc[stadium_df['team']==team]
 
   for segment in segments:
     ax.plot(stadium_graph_data.loc[stadium_df['segment']==segment,'x'],
             stadium_graph_data.loc[stadium_df['segment']==segment,'y'],
-            linewidth=round(scale_val_bat*1.5), color='black', alpha=0.8)
+            linewidth=linewidth, color='black', alpha=0.8)
