97f8748073eaa06149ee13e5e7c5388772967e16
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-05-13 00:03:34 -0600

    Update swing_speed.py

--- swing_speed.py
+++ swing_speed.py
@@ -119,7 +119,7 @@ def speed_dist(player,stat):
     xs_p = kdeline_p.get_xdata()
     ys_p = kdeline_p.get_ydata()
     height_p = np.interp(val, xs_p, ys_p)
-    ax.vlines(val, 0, height_p, color=player_color, ls='--')
+    ax.vlines(val, 0, height_p, color=player_color)
     
     ax.set_yticks([])
     title_stat = ' '.join(stat_name_dict[stat].split(' ')[:-1])
