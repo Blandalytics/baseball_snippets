97f84d292f6ca06adcc03d26ee89f61cbf96387c
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-05-15 22:43:16 -0600

    Update swing_speed.py

--- swing_speed.py
+++ swing_speed.py
@@ -428,7 +428,7 @@ def heatmap_data(df,stat):
     heatmap_df.loc[heatmap_df['sz_z'].notna(),'kde_z'] = np.clip(heatmap_df.loc[heatmap_df['sz_z'].notna(),'plate_z'].astype('float').mul(12).round(0).astype('int').div(12),
                                                  0,
                                                  4.5)
-    return heatmap_df['Hitter','stand',[heatmap_stat_dict[stat][0],'plate_x','sz_z','kde_x','kde_z']]
+    return heatmap_df[['Hitter','stand',heatmap_stat_dict[stat][0],'plate_x','sz_z','kde_x','kde_z']]
 
 def swing_heatmap(df,hitter,base_stat):
     b_hand = df.loc[(df['Hitter']==hitter),'stand'].value_counts().index[0]
