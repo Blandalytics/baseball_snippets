d2fc54294547f0f9487884d6fa3541ed5a5dcf7f
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-05-17 10:29:36 -0600

    Update swing_speed.py

--- swing_speed.py
+++ swing_speed.py
@@ -551,14 +551,14 @@ def swing_heatmap(df,hitter,base_stat):
         fig.text(0.785,0.06,'@blandalytics\nData: Baseball Savant',ha='center',fontsize=6)
         fig.text(0.215,0.06,'mlb-swing-speed\n.streamlit.app',ha='center',fontsize=6)
         # fig.text(0.5,0.85,f'Blue is {stat_value_dict[stat][0]} than Lg Avg, Red is {stat_value_dict[stat][1]}',ha='center',fontsize=10, color='k')
-        base_text = ax.text(20,50,f'Blue is {stat_value_dict[stat][0]} than Lg Avg, Red is {stat_value_dict[stat][1]}',alpha=0,ha='center')
+        base_text = ax.text(20,52,f'Blue is {stat_value_dict[stat][0]} than Lg Avg, Red is {stat_value_dict[stat][1]}',alpha=0,ha='center')
         scale_text = ax.annotate('Blue',color=sns.color_palette('vlag',n_colors=1000)[0],ha='left',fontweight='bold',
                                  fontsize=10, xycoords=base_text, xy=(0, 0), va="bottom")
-        scale_text = ax.annotate(f' is {stat_value_dict[stat][0]} than Lg Avg, ', 
+        scale_text = ax.annotate(f' is {stat_value_dict[stat][0]} than Lg Avg, ', fontsize=10,
                                  xycoords=scale_text, xy=(1, 0), va="bottom",color="k")
-        scale_text = ax.annotate('Red', xycoords=scale_text, xy=(1, 0), va="bottom",
+        scale_text = ax.annotate('Red', xycoords=scale_text, xy=(1, 0), va="bottom", fontsize=10,
                                  color=sns.color_palette('vlag',n_colors=1000)[-1], fontweight='bold')
-        scale_text = ax.annotate(f' is {stat_value_dict[stat][1]}', xycoords=scale_text, 
+        scale_text = ax.annotate(f' is {stat_value_dict[stat][1]}', xycoords=scale_text, fontsize=10,
                                  xy=(1, 0), va="bottom",color="k")
         sns.despine(left=True,bottom=True)
         st.pyplot(fig)
