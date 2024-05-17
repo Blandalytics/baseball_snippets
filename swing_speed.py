ae2a193de44cfc850e459fa7bf088f0af6c96acb
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-05-17 15:52:23 -0600

    Update swing_speed.py

--- swing_speed.py
+++ swing_speed.py
@@ -274,7 +274,7 @@ def speed_dist(swing_data,player,stat,handedness):
     hand_text = '' if handedness=='All' else f'\nAs {hand}HH'
     date_text = '' if (start_date==season_start) & (end_date==season_end) else f' ({start_date:%b %-d} - {end_date:%b %-d})'
     count_text = '' if count_select=='All' else f'\nIn {count_select} counts' if handedness=='All' else f' in {count_select} counts'
-    fig.suptitle(f"{player}{apostrophe_text}\n{title_stat}{date_text}{hand_text}{count_text}",y=1.025 if count_select=='All' else 1.075)
+    fig.suptitle(f"{player}{apostrophe_text}\n{title_stat}{date_text}{hand_text}{count_text}",y=1.025 if (count_select=='All') and (handedness=='All') else 1.075)
     sns.despine(left=True)
     fig.text(0.8,-0.15,'@blandalytics\nData: Savant',ha='center',fontsize=10)
     fig.text(0.125,-0.14,'mlb-swing-speed.streamlit.app',ha='left',fontsize=10)
