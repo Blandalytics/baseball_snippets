a8e3737ea5c4d4c4742389e54ba590cbded4089a
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-05-17 10:25:25 -0600

    Update swing_speed.py

--- swing_speed.py
+++ swing_speed.py
@@ -550,7 +550,28 @@ def swing_heatmap(df,hitter,base_stat):
         fig.suptitle(f"{hitter}{apostrophe_text}\n{stat_dict[stat][0]} Heatmap (vs Lg Avg)",y=0.96)
         fig.text(0.785,0.06,'@blandalytics\nData: Baseball Savant',ha='center',fontsize=6)
         fig.text(0.215,0.06,'mlb-swing-speed\n.streamlit.app',ha='center',fontsize=6)
-        fig.text(0.5,0.85,f'Blue is {stat_value_dict[stat][0]} than Lg Avg, Red is {stat_value_dict[stat][1]}',ha='center',fontsize=10, color='k')
+        # fig.text(0.5,0.85,f'Blue is {stat_value_dict[stat][0]} than Lg Avg, Red is {stat_value_dict[stat][1]}',ha='center',fontsize=10, color='k')
+        base_text = ax.text(125,0.85,f'Blue is {stat_value_dict[stat][0]} than Lg Avg, Red is {stat_value_dict[stat][1]}',alpha=0,ha='center')
+        scale_text = ax.annotate('Blue',color=sns.color_palette('vlag',n_colors=1000)[0],ha='left',fontweight='bold',
+                                 fontsize=10, xycoords=base_text, xy=(0, 0), va="bottom")
+        scale_text = ax.annotate(f' is {stat_value_dict[stat][0]} than Lg Avg, ', 
+                                 xycoords=scale_text, xy=(1, 0), va="bottom",color="k")
+        scale_text = ax.annotate('Red', xycoords=scale_text, xy=(1, 0), va="bottom",
+                                 color=sns.color_palette('vlag',n_colors=1000)[-1], fontweight='bold')
+        scale_text = ax.annotate(f' is {stat_value_dict[stat][1]}', xycoords=scale_text, 
+                                 xy=(1, 0), va="bottom",color="k")
+
+        text = ax.text(.1, .5, "Matplotlib", color="red")
+        # Subsequent words, positioned with annotate(), relative to the preceding one.
+        text = ax.annotate(
+            " says,", xycoords=text, xy=(1, 0), verticalalignment="bottom",
+            color="gold", weight="bold")  # custom properties
+        text = ax.annotate(
+            " hello", xycoords=text, xy=(1, 0), verticalalignment="bottom",
+            color="green", style="italic")  # custom properties
+        text = ax.annotate(
+            " world!", xycoords=text, xy=(1, 0), verticalalignment="bottom",
+            color="blue", family="serif")  # custom properties
         sns.despine(left=True,bottom=True)
         st.pyplot(fig)
 swing_heatmap(heatmap_data(swing_data,stat),player,stat)
