2a73a6d1f6672500256370954f9e0f18c2b43b7c
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-08-01 14:52:03 -0600

    Create medal_count.py

---
+++ medal_count.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def load_data():
+  tables=pd.read_html("https://en.wikipedia.org/wiki/2024_Summer_Olympics_medal_table")
+  return tables[3].iloc[:-1].copy()
+
+medal_df = load_data().rename(columns={'NOC':'Country','Total':'Medal Count'})
+
+gold_vs_silver_weight = st.slider(
+  "How much more valuable is a Gold Medal vs a Silver Medal?\n(2: Gold = 2x Silver)",
+  min_value=1,
+  max_value=50,
+  value=2,
+)
+silver_vs_bronze_weight =  st.slider(
+  "How much more valuable is a Gold Medal vs a Silver Medal?\n(2: Silver = 2x Bronze)",
+  min_value=1,
+  max_value=50,
+  value=2
+)
+gold_vs_bronze_weight = gold_vs_silver_weight * silver_vs_bronze_weight
+
+medal_df['weighted_Silver'] = medal_df['Silver'].div(gold_vs_silver_weight)
+medal_df['weighted_Bronze'] = medal_df['Bronze'].div(gold_vs_bronze_weight)
+medal_df['Weighted Count'] = medal_df[['Gold','weighted_Silver','weighted_Bronze']].astype('float').sum(axis=1).round(1)
+
+st.dataframe(medal_df[['Country','Gold','Silver','Bronze','Medal Count','Weighted Count']].sort_values('Weighted Count',ascending=False).round({'Weighted Count':1}))
