12e1f199c605e74e8c291463f8dc4159eb8897c7
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-08-01 20:51:23 -0600

    Update medal_count.py

--- medal_count.py
+++ medal_count.py
@@ -46,45 +46,42 @@ for medal_count in ['weighted_Bronze','weighted_Silver','weighted_Gold','Weighte
 
 cmap = colors.LinearSegmentedColormap.from_list('medal_colors', ['#977547','#d6d6d6','#fcb434'], N=3)
 
-col3, col4 = st.columns(2)
-with col3:
-    fig, ax = plt.subplots(figsize=(6,5))
-    medal_df.sort_values('Weighted Count',ascending=False).set_index('Country')[['weighted_Bronze','weighted_Silver','weighted_Gold']].head(5).round(1).plot(kind='bar', 
-                                                                               cmap=cmap, 
-                                                                                stacked=True,
-                                                                                                                                  edgecolor='w',
-                                                                               ax=ax, linewidth=2,
-                                                                               width=0.7,
-                                                                                                                                 legend=False)
-    for container in ax.containers[2::3]:
-        ax.bar_label(container)
-      
-    new_patches = []
-    for patch in reversed(ax.patches):
-        bb = patch.get_bbox()
-        color=patch.get_facecolor()
-        p_bbox = FancyBboxPatch((bb.xmin, bb.ymin),
-                            abs(bb.width), abs(bb.height),
-                            boxstyle="round,pad=-0.015,rounding_size=0.075",
-                            ec="none", fc=color,
-                            mutation_aspect=4
-                            )
-        patch.remove()
-        new_patches.append(p_bbox)
-    for patch in new_patches:
-        ax.add_patch(patch)
-    
-    ax.set(xlabel='',
-          ylabel='Weighted Medals')
-    fig.suptitle('Olympic Medal Leaders',fontsize=18)
-    fig.text(0.5,0.925,f'(Weights: Gold = {gold_vs_silver_weight}x Silver, Silver = {silver_vs_bronze_weight}x Bronze)',ha='center',va='top')
-    fig.text(0.04,0.025,'Data: Wikipedia',ha='left',va='center',fontsize=8)
-    fig.text(0.9,0.025,'* Host Nation',ha='right',va='center',fontsize=8)
-    plt.xticks(rotation=0)
-    sns.despine(left=True,bottom=True)
-    st.pyplot(fig)
+fig, ax = plt.subplots(figsize=(6,5))
+medal_df.sort_values('Weighted Count',ascending=False).set_index('Country')[['weighted_Bronze','weighted_Silver','weighted_Gold']].head(5).round(1).plot(kind='bar', 
+                                                                           cmap=cmap, 
+                                                                            stacked=True,
+                                                                                                                              edgecolor='w',
+                                                                           ax=ax, linewidth=2,
+                                                                           width=0.7,
+                                                                                                                             legend=False)
+for container in ax.containers[2::3]:
+    ax.bar_label(container)
   
-with col4:
-    st.dataframe(medal_df[['Country','Gold','Silver','Bronze','Medals','Weighted Count']].sort_values('Weighted Count',ascending=False).round({'Weighted Count':1}),
-                 hide_index=True,
-                 height=(medal_df.shape[0] + 1) * 35 + 3)   
+new_patches = []
+for patch in reversed(ax.patches):
+    bb = patch.get_bbox()
+    color=patch.get_facecolor()
+    p_bbox = FancyBboxPatch((bb.xmin, bb.ymin),
+                        abs(bb.width), abs(bb.height),
+                        boxstyle="round,pad=-0.015,rounding_size=0.075",
+                        ec="none", fc=color,
+                        mutation_aspect=4
+                        )
+    patch.remove()
+    new_patches.append(p_bbox)
+for patch in new_patches:
+    ax.add_patch(patch)
+
+ax.set(xlabel='',
+      ylabel='Weighted Medals')
+fig.suptitle('Olympic Medal Leaders',fontsize=18)
+fig.text(0.5,0.925,f'(Weights: Gold = {gold_vs_silver_weight}x Silver, Silver = {silver_vs_bronze_weight}x Bronze)',ha='center',va='top')
+fig.text(0.04,0.025,'Data: Wikipedia',ha='left',va='center',fontsize=8)
+fig.text(0.9,0.025,'* Host Nation',ha='right',va='center',fontsize=8)
+plt.xticks(rotation=0)
+sns.despine(left=True,bottom=True)
+st.pyplot(fig)
+
+st.dataframe(medal_df[['Country','Gold','Silver','Bronze','Medals','Weighted Count']].sort_values('Weighted Count',ascending=False).round({'Weighted Count':1}),
+             hide_index=True,
+             height=(medal_df.shape[0] + 1) * 35 + 3)   
