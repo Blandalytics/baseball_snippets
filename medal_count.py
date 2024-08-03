81bf3bc241effa8bd96ebcc41bfb573f7f22b018
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-08-03 09:23:25 -0600

    Update medal_count.py

--- medal_count.py
+++ medal_count.py
@@ -88,8 +88,8 @@ for color in medal_dict.keys():
     sns.scatterplot(x=medal_x,
                     y=medal_y,
                    color=medal_dict[color][2],
-                    linewidth=0.5,
-                    edgecolor='#666666',
+                    # linewidth=0.5,
+                    # edgecolor='#666666',
                    s=1000/(row_medals**2),
                    ax=medal_ax)
     medal_ax.set(xlim=(medal_ax.get_xlim()[0]-1.5,medal_ax.get_xlim()[1]+1.5),
