adb111e06c62233f796b3595a634b6ff8c066aac
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-08-03 09:19:13 -0600

    Update medal_count.py

--- medal_count.py
+++ medal_count.py
@@ -99,7 +99,7 @@ for color in medal_dict.keys():
 
 ax.set(xlabel='',
       ylabel='Weighted Medals')
-fig.suptitle('Olympic Medal Leaders',fontsize=18,y=1.1)
+fig.suptitle('Olympic Medal Leaders',fontsize=18,y=1.05)
 fig.text(0.5,0.99,'=      =',ha='center',va='top',fontsize=36)
 fig.text(0.04,0.025,'Data: Wikipedia',ha='left',va='center',fontsize=8)
 fig.text(0.9,0.025,'* Host Nation',ha='right',va='center',fontsize=8)
