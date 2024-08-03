970d48b30e343ade5370b0f17ddf59f6e71a5150
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-08-03 09:54:50 -0600

    Update medal_count.py

--- medal_count.py
+++ medal_count.py
@@ -75,9 +75,9 @@ for patch in new_patches:
 plt.xticks(rotation=0)
 
 medal_dict = {
-    'Gold':[fig.add_axes([0.19,.8,0.2,0.2], anchor='SW', zorder=2), 1, '#fcb434'],
-    'Silver':[fig.add_axes([0.4,.8,0.2,0.2], anchor='SW', zorder=2), gold_vs_silver_weight, '#d6d6d6'],
-    'Bronze':[fig.add_axes([0.625,.8,0.2,0.2], anchor='SW', zorder=2), gold_vs_silver_weight*silver_vs_bronze_weight, '#977547']
+    'Gold':[fig.add_axes([0.19,.81,0.2,0.2], anchor='SW', zorder=2), 1, '#fcb434'],
+    'Silver':[fig.add_axes([0.4,.81,0.2,0.2], anchor='SW', zorder=2), gold_vs_silver_weight, '#d6d6d6'],
+    'Bronze':[fig.add_axes([0.625,.81,0.2,0.2], anchor='SW', zorder=2), gold_vs_silver_weight*silver_vs_bronze_weight, '#977547']
 }
 for color in medal_dict.keys():
     medal_ax = medal_dict[color][0]
@@ -100,7 +100,7 @@ for color in medal_dict.keys():
 ax.set(xlabel='',
       ylabel='Weighted Medals')
 fig.suptitle('Olympic Medal Leaders',fontsize=18,y=1.05)
-fig.text(0.35,0.94,'=       =',ha='left',va='top',fontsize=32)
+fig.text(0.35,0.95,'=       =',ha='left',va='top',fontsize=32)
 fig.text(0.04,0.025,'Data: Wikipedia',ha='left',va='center',fontsize=8)
 fig.text(0.9,0.025,'* Host Nation',ha='right',va='center',fontsize=8)
 
