6d8840cd7bd46457a7d9e29044ab2a72eb11a37f
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-09-11 00:46:05 -0600

    Update win_prob.py

--- win_prob.py
+++ win_prob.py
@@ -1,20 +1,43 @@
-import streamlit as st
 import datetime
 import random
 from datetime import timedelta
-import matplotlib as mpl
 import numpy as np
 import pandas as pd
-import pickle
 import seaborn as sns
 import requests
-import plotly.graph_objs as go
-from plotly import tools
-from plotly.subplots import make_subplots
-import plotly.offline as py
 import pytz
+import matplotlib as mpl
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+from matplotlib.collections import LineCollection
+import urllib
+import cairosvg
+from PIL import Image
 
-date = datetime.date(2024,9,10)
+chart_white = '#FEFEFE'
+chart_accent = '#162B50'
+
+sns.set_theme(
+    style={
+        'axes.edgecolor': chart_accent,
+        'axes.facecolor': chart_white,
+        'axes.labelcolor': chart_white,
+        'xtick.color': chart_accent,
+        'ytick.color': chart_accent,
+        'figure.facecolor':chart_white,
+        'grid.color': chart_white,
+        'grid.linestyle': '-',
+        'legend.facecolor':chart_white,
+        'text.color': 'k'
+     }
+    )
+
+color_df = pd.read_csv('https://github.com/Blandalytics/PLV_viz/blob/main/mlb_team_colors.csv?raw=true')
+color_dict = color_df[['Short Code','Color 1']].set_index('Short Code').to_dict()['Color 1']
+logo_dict = color_df[['Short Code','Logo']].set_index('Short Code').to_dict()['Logo']
+
+date = (datetime.datetime.now(pytz.utc)-timedelta(hours=16)).date()
 level=1
 
 r = requests.get(f'https://statsapi.mlb.com/api/v1/schedule?sportId={level}&date={date}')
@@ -158,6 +181,58 @@ dydx = 0.5 * (yinterp[:-1] + yinterp[1:])
 sns.lineplot(x=np.array(xvals), 
              y=yinterp, color='#aaaaaa', linewidth=6.5,
                     )
+
+def colored_line_between_pts(x, y, c, ax, **lc_kwargs):
+    """
+    Plot a line with a color specified between (x, y) points by a third value.
+
+    It does this by creating a collection of line segments between each pair of
+    neighboring points. The color of each segment is determined by the
+    made up of two straight lines each connecting the current (x, y) point to the
+    midpoints of the lines connecting the current point with its two neighbors.
+    This creates a smooth line with no gaps between the line segments.
+
+    Parameters
+    ----------
+    x, y : array-like
+        The horizontal and vertical coordinates of the data points.
+    c : array-like
+        The color values, which should have a size one less than that of x and y.
+    ax : Axes
+        Axis object on which to plot the colored line.
+    **lc_kwargs
+        Any additional arguments to pass to matplotlib.collections.LineCollection
+        constructor. This should not include the array keyword argument because
+        that is set to the color argument. If provided, it will be overridden.
+
+    Returns
+    -------
+    matplotlib.collections.LineCollection
+        The generated line collection representing the colored line.
+    """
+    if "array" in lc_kwargs:
+        warnings.warn('The provided "array" keyword argument will be overridden')
+
+    # Check color array size (LineCollection still works, but values are unused)
+    if len(c) != len(x) - 1:
+        warnings.warn(
+            "The c argument should have a length one less than the length of x and y. "
+            "If it has the same length, use the colored_line function instead."
+        )
+
+    # Create a set of line segments so that we can color them individually
+    # This creates the points as an N x 1 x 2 array so that we can stack points
+    # together easily to get the segments. The segments array for line collection
+    # needs to be (numlines) x (points per line) x 2 (for x and y)
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    lc = LineCollection(segments, **lc_kwargs)
+
+    # Set the values used for colormapping
+    lc.set_array(c)
+
+    return ax.add_collection(lc)
+
 lines = colored_line_between_pts(np.array(xvals), 
                                  yinterp, 
                                  dydx,
