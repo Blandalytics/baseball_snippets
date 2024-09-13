5fde165f12e73fcada4d732fa640111a7f5e4378
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-09-13 14:54:06 -0600

    Update wheeee_index.py

--- wheeee_index.py
+++ wheeee_index.py
@@ -268,7 +268,7 @@ st.dataframe(all_games_df
 # st.write(data.selected_rows['game_name'][-6:])
 # game_choice_id = int(data.selected_rows['game_name'].values[0][-6:])
 
-game_choice = st.select_box('Choose a game:',game_list)
+game_choice = st.selectbox('Choose a game:',game_list)
 game_choice_id = int(game_choice[-6:])
 
 def game_chart(game_choice_id):
