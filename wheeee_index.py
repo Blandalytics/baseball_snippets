a86a20dafa397b078a4f89460dc50073ffad12b1
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-09-15 00:21:17 -0600

    Update wheeee_index.py

--- wheeee_index.py
+++ wheeee_index.py
@@ -335,7 +335,7 @@ def game_chart(game_choice_id):
             continue
         ax.text((inning+0.5)*6,0.5,inning+1,ha='center',va='center',
                 bbox=dict(boxstyle='round', facecolor=chart_white, alpha=0.75,edgecolor='k'))
-        ax.axvline((inning+1)*6,linestyle='--',alpha=0.25,ymin=(0.25+0.1)/1.4,ymax=(0.75+0.1)/1.4,color='k')
+        ax.axvline((inning+1)*6,linestyle='--',alpha=0.25,ymin=(0.25+0.1)/1.5,ymax=(0.75+0.1)/1.5,color='k')
 
     dydx = 0.5 * (yinterp[:-1] + yinterp[1:])
     sns.lineplot(x=np.array(xvals)[:-1], 
