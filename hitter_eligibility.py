da7a6b18b6b275853d4dc9531e76ac575efa279b
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-09-24 21:32:12 -0600

    Update hitter_eligibility.py

--- hitter_eligibility.py
+++ hitter_eligibility.py
@@ -36,7 +36,8 @@ st.dataframe(pivot_df,
 players = list(pivot_df['name'].unique())
 default_val = players.index('Aaron Judge')
 player_select = st.selectbox('Choose a hitter:', pivot_df['name'].unique(), index=default_val)
+pos_text = ', '.join(player_data.loc[player_data['name']==player_select,'position'].to_list())
 st.write(f"""
 {player_select}'s Eligible Positions (min {games_started_thresh} starts or {games_played_thresh} appearances):
-', '.join(player_data.loc[player_data['name']==player_select,'position'].to_list()))
+{pos_text}
 """)
