f801958c4e540f7b6883476a11e4e62239eb5ff2
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-10-06 23:22:59 -0600

    Update mlb_series_win.py

--- mlb_series_win.py
+++ mlb_series_win.py
@@ -66,7 +66,7 @@ fill_dict = {1:est_win_prob+hfa}
 fill_dict.update({x*2+1:sum(best_of_prob(x*2+1,est_win_prob,sims,hfa=hfa)[0])/sims for x in range(1,6)})
 
 def series_chart(fill_dict):
-    fig, ax = plt.subplots(figsize=(6,4))
+    fig, ax = plt.subplots(figsize=(6,3))
     sns.lineplot(fill_dict,color=team_df[team_df['Team']==underdog]['Color'].values[0])
     for series_len in fill_dict.keys():
         ax.text(series_len,fill_dict[series_len],
@@ -77,8 +77,7 @@ def series_chart(fill_dict):
                bbox=dict(boxstyle="round",pad=0.25,alpha=1,edgecolor=team_df[team_df['Team']==underdog]['Color'].values[0],
                          color=team_df[team_df['Team']==favored_team]['Color'].values[0]))
     ax.set_xticks(list(fill_dict.keys()))
-    # ax.yaxis.set_major_formatter(mtick.PercentFormatter(1))
-    ax.yaxis.set_visible(False)
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(1))
     ax.set(xlim=(0,max(fill_dict.keys())+1),
            ylim=(0.5,1),
            xlabel='Series Length ("Best of X")')
