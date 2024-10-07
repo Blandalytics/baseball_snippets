cef8f47e5df681258320b5e9a11a95fbb287d3a4
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-10-06 21:11:34 -0600

    Update mlb_series_win.py

--- mlb_series_win.py
+++ mlb_series_win.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import seaborn as sns
 
+sims = 10000
+
 def log_pythag_win(favorite_rs,favorite_ra,
                   underdog_rs,underdog_ra):
     favorite_factor = (favorite_rs+favorite_ra)**0.287
@@ -15,7 +17,7 @@ def log_pythag_win(favorite_rs,favorite_ra,
 
     return (favorite_win_prob-(favorite_win_prob*underdog_win_prob))/(favorite_win_prob+underdog_win_prob-(2 * favorite_win_prob*underdog_win_prob))
 def best_of_prob(games, favorite_win_prob, 
-                 sims=10000, hfa=0.04):
+                 sims=sims, hfa=0.04):
     series_schedule = [1,0,1] if games==3 else [1,1,0,0] + [1,0]*int(round((games-5)/2)) + [1]
     series_wins = []
     series_games = []
@@ -65,7 +67,7 @@ else:
 
 st.write(f'The {favored_team} are expected to beat the {underdog} {est_win_prob:.1%} of the time.')
 fill_dict = {1:est_win_prob+hfa}
-fill_dict.update({x*2+1:best_of_prob(x*2+1,est_win_prob,10000,hfa=hfa)[0] for x in range(1,6)})
+fill_dict.update({x*2+1:sum(best_of_prob(x*2+1,est_win_prob,10000,hfa=hfa)[0])/sims for x in range(1,6)})
 
 def series_chart(fill_dict):
     fig, ax = plt.subplots(figsize=(6,4))
@@ -92,6 +94,6 @@ series_chart(fill_dict)
 
 series_wins, series_games = best_of_prob(games=7,
                                          favorite_win_prob=est_win_prob,
-                                         sims=100000,
+                                         sims=sims,
                                          hfa=hfa)
-series_win_prob = sum(series_wins) / 100000
+series_win_prob = sum(series_wins) / sims
