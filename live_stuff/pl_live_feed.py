1c2146d89f8af57dac624bcbcbf684827483c66b
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-10-15 11:48:06 -0600

    Update pl_live_feed.py

--- live_stuff/pl_live_feed.py
+++ live_stuff/pl_live_feed.py
@@ -479,11 +479,6 @@ category_feats = ['p_throws_L', 'stand_L',
                   'balls_1','balls_2','balls_3',
                   'strikes_1','strikes_2'
                  ]
-
-for x in category_feats:
-    if x not in chart_df.columns.values:
-        chart_df[x] = None
-
 stuff_feats = ['velo','velo_diff',
                'extension',
                'spin_rate',
