ed3fda5035b00714971a0fd66c4eb560411cbd47
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-01-28 22:24:26 -0800

    Update nfbc_adp.py

--- nfbc_adp.py
+++ nfbc_adp.py
@@ -100,20 +100,20 @@ def plot_draft_data(df,player,start_date):
            ha='left',va='center')
   
   min_val = chart_df.iloc[-1]['Min Pick']
-  if min_val <= adp_val*0.9:
+  if min_val <= adp_val*0.95:
     ax.text(chart_df.iloc[-1]['end_date'] + datetime.timedelta(days=2),
             min_val,
             f'Min Pick: {min_val:.0f}',
              ha='left',va='center', color=chart_red)
   
   max_val = chart_df.iloc[-1]['Max Pick']
-  if max_val >= adp_val*1.2:
+  if max_val >= adp_val*1.1:
     ax.text(chart_df.iloc[-1]['end_date'] + datetime.timedelta(days=2),
             max_val,
             f'Max Pick: {max_val:.0f}',
              ha='left',va='center',color=chart_blue)
     
-  if chart_df.iloc[-1]['ADP'] + chart_df.iloc[-1]['StDev Est'] >= adp_val*1.1:
+  if chart_df.iloc[-1]['ADP'] + chart_df.iloc[-1]['StDev Est'] >= adp_val*1.05:
     ax.text(chart_df.iloc[-1]['end_date'] + datetime.timedelta(days=2),
             chart_df.iloc[-1]['ADP'] + chart_df.iloc[-1]['StDev Est'],
             'St Dev (est)',
