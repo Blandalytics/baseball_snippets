af544e4c60fdfdbddf76284d00939aea6f62e46f
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-07-22 01:01:17 -0700

    Update swing_speed.py

--- swing_speed.py
+++ swing_speed.py
@@ -11,7 +11,7 @@ from PIL import Image
 from statsmodels.nonparametric.kernel_regression import KernelReg
 
 st.title('MLB Swing Speed App')
-st.write('Find me [@Blandalytics](https://twitter.com/blandalytics) and check out the data at [Baseball Savant](https://baseballsavant.mlb.com/leaderboard/bat-tracking)')
+st.write('Find me [@Blandalytics](https://bsky.app/profile/blandalytics.pitcherlist.com) and check out the data at [Baseball Savant](https://baseballsavant.mlb.com/leaderboard/bat-tracking)')
 st.write('Please check the assumptions at the bottom of the page, as there are some that are pretty significant for calculating Swing Time & Swing Acceleration')
 st.write('NOTE: Bat Speed measurements are [capped at 88mph](https://x.com/tangotiger/status/1838630623373615459) (for the public data)')
 pl_white = '#FEFEFE'
