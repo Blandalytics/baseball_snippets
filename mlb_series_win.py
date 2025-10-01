6ffc96dd106c1f32be668f2f5b0eae23b79c040e
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-10-01 10:02:29 -0700

    Update mlb_series_win.py

--- mlb_series_win.py
+++ mlb_series_win.py
@@ -18,17 +18,17 @@ st.title('MLB Series Simulator')
 sims = 100000
 max_series_len = 15
 
-def log_pythag_win(favorite_rs,favorite_ra,
-                  underdog_rs,underdog_ra):
-    favorite_factor = (favorite_rs+favorite_ra)**0.285
-    favorite_win_prob = favorite_rs**favorite_factor/(favorite_rs**favorite_factor+favorite_ra**favorite_factor)
+def log_pythag_win(higher_rs,higher_ra,
+                  lower_rs,lower_ra):
+    higher_factor = (higher_rs+higher_ra)**0.285
+    higher_win_prob = higher_rs**higher_factor/(higher_rs**higher_factor+higher_ra**higher_factor)
     
-    underdog_factor = (underdog_rs+underdog_ra)**0.285
-    underdog_win_prob = underdog_rs**underdog_factor/(underdog_rs**underdog_factor+underdog_ra**underdog_factor)
+    lower_factor = (lower_rs+lower_ra)**0.285
+    lower_win_prob = lower_rs**lower_factor/(lower_rs**lower_factor+lower_ra**lower_factor)
 
-    return (favorite_win_prob-(favorite_win_prob*underdog_win_prob))/(favorite_win_prob+underdog_win_prob-(2 * favorite_win_prob*underdog_win_prob))
+    return (higher_win_prob-(higher_win_prob*lower_win_prob))/(higher_win_prob+lower_win_prob-(2 * higher_win_prob*lower_win_prob))
                     
-def best_of_prob(games, favorite_win_prob, 
+def best_of_prob(games, higher_seed_win_prob, 
                  sims=sims, hfa=0.04,all_home=False):
     if all_home:
         series_schedule = [1] * games
@@ -37,21 +37,21 @@ def best_of_prob(games, favorite_win_prob,
     series_wins = []
     series_games = []
     for series in range(sims):
-        favored_wins = 0
-        underdog_wins = 0
+        higher_seed_wins = 0
+        lower_seed_wins = 0
         series_win = 0
         games_played = 0
         for game in series_schedule:
-            win_prob = favorite_win_prob+hfa*(game-0.5)*2
+            win_prob = higher_seed_win_prob+hfa*(game-0.5)*2
             games_played += 1
             if np.random.random() <=win_prob:
-                favored_wins += 1
+                higher_seed_wins += 1
             else:
-                underdog_wins += 1
-            if favored_wins == int(games/2+0.5):
+                lower_seed_wins += 1
+            if higher_seed_wins == int(games/2+0.5):
                 series_win = 1
                 break
-            if underdog_wins == int(games/2+0.5):
+            if lower_seed_wins == int(games/2+0.5):
                 break
         series_games += [games_played]
         series_wins += [series_win]
@@ -64,24 +64,24 @@ team_df = pd.read_csv('https://docs.google.com/spreadsheets/d/1zbYqHg685OyP_D-E_
 
 col1, col2 = st.columns(2)
 with col1:
-    team_1 = st.selectbox('Higher seed:',list(team_df['Team']),index=1)
+    higher_seed_team = st.selectbox('Higher seed:',list(team_df['Team']),index=1)
 with col2:
-    team_2 = st.selectbox('Lower seed:',list(team_df['Team']),index=5)
+    lower_seed_team = st.selectbox('Lower seed:',list(team_df['Team']),index=5)
 
-favored_team, underdog = (team_1, team_2) if team_df[team_df['Team']==team_1]['Win%'].values[0] >= team_df[team_df['Team']==team_2]['Win%'].values[0] else (team_2, team_1)
+# higher_seed_team, lower_seed_team = (team_1, team_2) if team_df[team_df['Team']==team_1]['Win%'].values[0] >= team_df[team_df['Team']==team_2]['Win%'].values[0] else (team_2, team_1)
 
-favorite_rs,favorite_ra = team_df[team_df['Team']==favored_team][['Runs Scored','Runs Allowed']].values[0]
-favored_color = team_df[team_df['Team']==favored_team]['Color'].values[0]
-underdog_rs,underdog_ra = team_df[team_df['Team']==underdog][['Runs Scored','Runs Allowed']].values[0]
-underdog_color = team_df[team_df['Team']==underdog]['Color'].values[0]
+higher_seed_rs,higher_seed_ra = team_df[team_df['Team']==higher_seed_team][['Runs Scored','Runs Allowed']].values[0]
+higher_seed_color = team_df[team_df['Team']==higher_seed_team]['Color'].values[0]
+lower_seed_rs,lower_seed_ra = team_df[team_df['Team']==lower_seed_team][['Runs Scored','Runs Allowed']].values[0]
+lower_seed_color = team_df[team_df['Team']==lower_seed_team]['Color'].values[0]
 hfa = 0.04
 
 if team_1==team_2:
     est_win_prob = 0.5
 else:
-    est_win_prob = log_pythag_win(favorite_rs,favorite_ra,
-                                  underdog_rs,underdog_ra)
-st.write(f'The {favored_team} are expected to beat the {underdog} ~{est_win_prob:.1%} of the time at a neutral site, based on their regular season runs scored and allowed. Home Field Advantage is assumed to be worth ~{hfa:.0%}.')
+    est_win_prob = log_pythag_win(higher_seed_rs,higher_seed_ra,
+                                  lower_seed_rs,lower_seed_ra)
+st.write(f'The {higher_seed_team} are expected to beat the {lower_seed_team} ~{est_win_prob:.1%} of the time at a neutral site, based on their regular season runs scored and allowed. Home Field Advantage is assumed to be worth ~{hfa:.0%}.')
 
 @st.cache_data(ttl=10*60,show_spinner=f"Simulating {sims:,} matchups, for each series length")
 def series_sims(est_win_prob,sims,hfa,series_max=max_series_len):
@@ -91,7 +91,7 @@ def series_sims(est_win_prob,sims,hfa,series_max=max_series_len):
 
 def series_chart(fill_dict):
     fig, ax = plt.subplots(figsize=(6,4))
-    sns.lineplot(fill_dict,color=underdog_color,linewidth=3)
+    sns.lineplot(fill_dict,color=lower_seed_color,linewidth=3)
     for series_len in fill_dict.keys():
         ax.text(series_len,fill_dict[series_len],
                 f'{fill_dict[series_len]:.1%}' if round(fill_dict[series_len],3)<1 else '~100%',
@@ -99,55 +99,20 @@ def series_chart(fill_dict):
                 color='w',
                 ha='center',va='center',
                bbox=dict(boxstyle="round",pad=0.25,alpha=1,
-                         edgecolor=underdog_color,linewidth=1,
-                         color=favored_color))
+                         edgecolor=lower_seed_color,linewidth=1,
+                         color=higher_seed_color))
     ax.set_xticks(list(fill_dict.keys()))
     ax.yaxis.set_major_formatter(mtick.PercentFormatter(1))
     ax.set(xlim=(0,max(fill_dict.keys())+1),
-           ylim=(0.5,1),
+           ylim=(0.5,1) if team_df[team_df['Team']==higher_seed_team]['Win%'].values[0] >= team_df[team_df['Team']==lower_seed_team]['Win%'].values[0] else (0,0.6),
            xlabel='Series Length ("Best of X")')
-    fig.suptitle(f'{favored_team} over {underdog} Series Win%\n(Single Game, Neutral Site xWin%: {est_win_prob:.1%})',
+    fig.suptitle(f'{higher_seed_team} over {lower_seed_team} Series Win%\n(Single Game, Neutral Site xWin%: {est_win_prob:.1%})',
                 y=1.02)
     pl_ax = fig.add_axes([0.03,-0.015,0.2,0.1], anchor='NE', zorder=1)
     width, height = logo.size
     pl_ax.imshow(logo.crop((0, 0, width, height-150)))
     pl_ax.axis('off')
     sns.despine(bottom=True)
-    st.pyplot(fig)
-
-def games_played_chart(series_len):
-    games = best_of_prob(series_len,est_win_prob,
-                         sims,hfa=hfa,all_home=all_home)
-    font_size = np.clip(120/series_len,6,12)
-    fig, ax  = plt.subplots(figsize=(6,4))
-    game_space = list(set(games[1]))
-    sns.histplot(x=games[1], 
-                 hue=games[0],
-                 palette=[underdog_color,favored_color],
-                 stat='percent',multiple='stack',binrange=(min(game_space)-0.5,max(game_space)+0.5),binwidth=1,
-                 alpha=1,
-                 edgecolor='w')
-    for p in ax.patches:
-        height_check = p.get_height() + 1 > ax.get_ylim()[1]/10
-        ax.annotate(f"{p.get_height():.1f}%\n" if p.get_height() >= 0.05 else '~0%\n', 
-                    (p.get_x() + p.get_width() / 2, 
-                     (p.get_y() + p.get_height()/2 - 1) if height_check else (p.get_y() + p.get_height() - ax.get_ylim()[1]/30)),
-                    ha="center", 
-                     va="center" if height_check else "bottom",
-                     color='w' if height_check else underdog_color,
-                    fontsize=font_size)
-    ax.legend(ncol=2,bbox_to_anchor=(0.49,1),loc='lower center',
-              labels=[favored_team+f' Win: {sum(games[0])/sims:.1%}',underdog+f' Win: {1-sum(games[0])/sims:.1%}'],edgecolor='w',framealpha=0)
-
-    ax.yaxis.set_major_formatter(mtick.PercentFormatter(100,0))
-    ax.set_xticks(game_space)
-    ax.set(xlabel='Win in X Games',ylabel='',ylim=(0,ax.get_ylim()[1]*1.02))
-    pl_ax = fig.add_axes([0.04,-0.015,0.2,0.1], anchor='NE', zorder=1)
-    width, height = logo.size
-    pl_ax.imshow(logo.crop((0, 0, width, height-150)))
-    pl_ax.axis('off')
-    fig.suptitle(f'{favored_team}/{underdog} Series\nGames Played Distribution (Best of {series_len})',y=1.06)
-    sns.despine()
     st.pyplot(fig)
 
 # if st.button("Simulate series"):
@@ -162,6 +127,41 @@ series_len = st.slider(
       step=2
   )
 all_home = st.checkbox("All higher seed home games")
+
+def games_played_chart(series_len):
+    games = best_of_prob(series_len,est_win_prob,
+                         sims,hfa=hfa,all_home=all_home)
+    font_size = np.clip(120/series_len,6,12)
+    fig, ax  = plt.subplots(figsize=(6,4))
+    game_space = list(set(games[1]))
+    sns.histplot(x=games[1], 
+                 hue=games[0],
+                 palette=[lower_seed_color,higher_seed_color],
+                 stat='percent',multiple='stack',binrange=(min(game_space)-0.5,max(game_space)+0.5),binwidth=1,
+                 alpha=1,
+                 edgecolor='w')
+    for p in ax.patches:
+        height_check = p.get_height() + 1 > ax.get_ylim()[1]/10
+        ax.annotate(f"{p.get_height():.1f}%\n" if p.get_height() >= 0.05 else '~0%\n', 
+                    (p.get_x() + p.get_width() / 2, 
+                     (p.get_y() + p.get_height()/2 - 1) if height_check else (p.get_y() + p.get_height() - ax.get_ylim()[1]/30)),
+                    ha="center", 
+                     va="center" if height_check else "bottom",
+                     color='w' if height_check else lower_seed_color,
+                    fontsize=font_size)
+    ax.legend(ncol=2,bbox_to_anchor=(0.49,1),loc='lower center',
+              labels=[higher_seed_team+f' Win: {sum(games[0])/sims:.1%}',lower_seed_team+f' Win: {1-sum(games[0])/sims:.1%}'],edgecolor='w',framealpha=0)
+
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(100,0))
+    ax.set_xticks(game_space)
+    ax.set(xlabel='Win in X Games',ylabel='',ylim=(0,ax.get_ylim()[1]*1.02))
+    pl_ax = fig.add_axes([0.04,-0.015,0.2,0.1], anchor='NE', zorder=1)
+    width, height = logo.size
+    pl_ax.imshow(logo.crop((0, 0, width, height-150)))
+    pl_ax.axis('off')
+    fig.suptitle(f'{higher_seed_team}/{lower_seed_team} Series\nGames Played Distribution (Best of {series_len})',y=1.06)
+    sns.despine()
+    st.pyplot(fig)
 games_played_chart(series_len)
 
 st.write(f'''
