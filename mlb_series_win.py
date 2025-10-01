c276d74707d74c21160e2b97ce06fe62c5bcec11
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-10-01 10:22:57 -0700

    Update mlb_series_win.py

--- mlb_series_win.py
+++ mlb_series_win.py
@@ -72,6 +72,7 @@ with col2:
 
 higher_seed_rs,higher_seed_ra = team_df[team_df['Team']==higher_seed_team][['Runs Scored','Runs Allowed']].values[0]
 higher_seed_color = team_df[team_df['Team']==higher_seed_team]['Color'].values[0]
+higher_seed_code = team_df[team_df['Team']==higher_seed_team]['Code'].values[0]
 lower_seed_rs,lower_seed_ra = team_df[team_df['Team']==lower_seed_team][['Runs Scored','Runs Allowed']].values[0]
 lower_seed_color = team_df[team_df['Team']==lower_seed_team]['Color'].values[0]
 hfa = 0.04
@@ -161,7 +162,8 @@ def games_played_chart(series_len):
     width, height = logo.size
     pl_ax.imshow(logo.crop((0, 0, width, height-150)))
     pl_ax.axis('off')
-    fig.suptitle(f'{higher_seed_team}/{lower_seed_team} Series\nGames Played Distribution (Best of {series_len})',y=1.06)
+    home_text = f', (all @{higher_seed_code})' if all_home else ''
+    fig.suptitle(f'{higher_seed_team}/{lower_seed_team} Series\nGames Played Distribution (Best of {series_len}{home_text})',y=1.06)
     sns.despine()
     st.pyplot(fig)
 games_played_chart(series_len)
