35a207ac96b2b6d78cf03508abe883bac658defc
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-12-13 01:28:35 -0800

    Update nfbc_adp.py

--- nfbc_adp.py
+++ nfbc_adp.py
@@ -249,7 +249,7 @@ def plot_draft_data(df,player,start_date):
   pl_ax.axis('off')
   
   fig.suptitle(f"{player}'s NFBC Draft Data\nRolling 14-Days: {chart_start} - {chart_end}",x=0.55,y=1,fontsize=14)
-  fig.text(0.47,1,f"{player}",ha='right',va='top',color='#F1C647',fontsize=14)
+  fig.text(0.475,1,f"{player}",ha='right',va='top',color='#F1C647',fontsize=14)
   sns.despine(left=True)
   st.pyplot(fig)
 
