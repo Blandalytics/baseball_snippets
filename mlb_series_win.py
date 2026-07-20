e18a67655435514381b5b1eee4bd22b9987a04d9
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-07-20 12:12:42 -0700

    Update mlb_series_win.py

--- mlb_series_win.py
+++ mlb_series_win.py
@@ -194,7 +194,9 @@ def games_played_chart(series_len):
         if height_check:
             ax.annotate(f"{p.get_height():.1f}%" if p.get_height() >= 0.05 else '~0%', 
                         (p.get_x() + p.get_width() / 2, 
-                         (p.get_y() + p.get_height()/2 - 1)),
+                         p.get_y()),
+                        # (p.get_x() + p.get_width() / 2, 
+                        #  (p.get_y() + p.get_height()/2 - 1)),
                         ha="center", 
                          va="center",
                          color='w',
