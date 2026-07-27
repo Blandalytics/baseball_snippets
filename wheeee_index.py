3511adaa3437e458422e3b9a219dde6eecccbf62
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-07-27 03:00:32 -0700

    Update wheeee_index.py

--- wheeee_index.py
+++ wheeee_index.py
@@ -841,7 +841,7 @@ def game_chart(game_choice_id):
         zorder=10,
     )
 
-    fig.text(f"{away_name} @ {home_name}", fontsize=25, x=0.415, y=0.9,ha="center")
+    fig.text(0.415,0.9,f"{away_name} @ {home_name}", fontsize=25,ha="center")
     fig.text(
         0.415,
         0.79,
